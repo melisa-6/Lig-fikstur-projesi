9c400e75817181d1b75f0c85f6ba822b598ee002
Melisa Taşkara <taskaramelisa@gmail.com>
2025-04-09 19:07:04 +0300

    Update fiksturolusturma.c

--- fiksturolusturma.c
+++ fiksturolusturma.c
@@ -5,29 +5,31 @@
 
 int bostakim;
 
-struct takim { //takimlari ve guclerini ayni anda bellekte tutmak icin struct yapisi tanimlanir
+struct takim {
     char takimadi[50];
     int guc;
-             };
+    int puan;  // Takimlarin puanlari
+};
 
 struct mac {
     int evsahibi;
     int rakip;
-            };
+};
 
 struct takim takimlar[19];
 struct mac eslesmeler[38][9];
 
- // dosyadaki takimlari okumasi ve diziye kaydetmesi icin fonksiyon olusturur
+// Dosyadaki takimlari okuma
 void takimlarioku() {
     FILE *dosya = fopen("takimlar.txt", "r");
     if (dosya == NULL) {
         printf("HATA: takimlar dosyasi acilamadi!\n");
-        exit(1);// Dosya acilmazsa durur
+        exit(1); // Dosya acilmazsa durur
     }
     for (int i = 0; i < 19; i++) {
-        fgets(takimlar[i].takimadi, sizeof(takimlar[i].takimadi), dosya);//dosyada okudugu takimlari struct icine atar
-        takimlar[i].takimadi[strcspn(takimlar[i].takimadi, "\n")] = '\0'; //\n karakterini  siler
+        fgets(takimlar[i].takimadi, sizeof(takimlar[i].takimadi), dosya);  // Takimlarin adlarini okur
+        takimlar[i].takimadi[strcspn(takimlar[i].takimadi, "\n")] = '\0';  // '\n' karakterini siler
+        takimlar[i].puan = 0;  // Baslangicta her takim 0 puanda
     }
     fclose(dosya);
 }
@@ -39,92 +41,122 @@ void takimguclerinioku() {
         exit(1); // Dosya acilmazsa durur
     }
     for (int j = 0; j < 19; j++) {    
-        fscanf(dosya, "%d", &takimlar[j].guc); //dosyada okudugu takimlarin guclerini struct icine atar
+        fscanf(dosya, "%d", &takimlar[j].guc);  // Her takim gucunu okur
     }
     fclose(dosya);
 }
-    // fikstur olusturmak icin fonksiyon baslatir
+
+// Fikstur olusturma
 void fiksturolustur() {
     srand(time(NULL));
-    for (int hafta = 0; hafta < 19; hafta++) { //1. haftadan 19. haftaya kadar rastgele ev sahibi rakip ve bos gecen takim secer
-        int eslesme[19] = {0}; //tum takimlari eslenmedi isaretler
+    FILE *dosya = fopen("fikstur.txt", "w");
+    if (dosya == NULL) {
+        printf("HATA: Fikstur dosyasi acilamadi!\n");
+        exit(1);
+    } 
+    for (int hafta = 0; hafta < 19; hafta++) {  // 19 hafta boyunca eslesme yapilir
+        int eslesme[19] = {0};  // Takimlarin eslesip eslesmedigini kontrol eder
 
-        bostakim = rand() % 19; //bos gecen takimi rastgele secer
-        eslesme[bostakim] = 1; // bos gecen takimi Secildi olarak isaretler
+        bostakim = rand() % 19;  // Bir takim bos gecilir
+        eslesme[bostakim] = 1;  // Bos gecilen takim isaretlenir
+        
+// Boş geçen takımı ekrana ve dosyaya yazdır
+printf("Hafta %d: %s takimı boş geçildi.\n", hafta + 1, takimlar[bostakim].takimadi);
+fprintf(dosya,"Hafta %d: %s takimı boş geçildi.\n", hafta + 1, takimlar[bostakim].takimadi);
 
         for (int macsayisi = 0; macsayisi < 9; macsayisi++) {
             int evsahibi, rakip;
             do { 
-                evsahibi = rand() % 19; //ev sahibi takimi rastgele secer
+                evsahibi = rand() % 19;  // Ev sahibi takim
             } while (eslesme[evsahibi] == 1);
-            eslesme[evsahibi] = 1; // ev sahibi takimi Secildi olarak isaretler
+            eslesme[evsahibi] = 1;
 
             do {
-                rakip = rand() % 19;//rakip  takimi rastgele secer
+                rakip = rand() % 19;  // Rakip takim
             } while (eslesme[rakip] == 1);
-            eslesme[rakip] = 1; // rakip takimi Secildi olarak isaretler
+            eslesme[rakip] = 1;
 
-            // ilk devre için eslesme
+            // Ilk devre icin eslesme
             eslesmeler[hafta][macsayisi].evsahibi = evsahibi;
             eslesmeler[hafta][macsayisi].rakip = rakip;
 
-            //ikinci devre icin eslesmeyi ters kaydeder yani 19 mac sonraya kaydeder boylece cift devreli olur
+            // Ikinci devre icin eslesme ters olarak kaydedilir
             eslesmeler[hafta + 19][macsayisi].evsahibi = rakip;
             eslesmeler[hafta + 19][macsayisi].rakip = evsahibi;
         }
     }
 }
-//takimlardaki guc farkin gore skor tahmini yapar
+// takimlardaki guc farkin gore skor tahmini yapar
 void macskortahmini(int evsahibi, int rakip, FILE *dosya) {
     int fark = takimlar[evsahibi].guc - takimlar[rakip].guc;
 
-    if (fark > 0) {  
-        if (fark >= 80) {
-            printf("Skor Tahmini: %s 5-6 farkla kazanabilir\n\n", takimlar[evsahibi].takimadi);
-            fprintf(dosya, "Skor Tahmini: %s 5-6 farkla kazanabilir\n\n", takimlar[evsahibi].takimadi);
-        } else if (fark >= 60) {
+    if (fark > 0) {  // Ev sahibi daha guclu ise
+        if (fark >= 60) {  // Buyuk fark
+            takimlar[evsahibi].puan += 3;  // Ev sahibi 3 puan kazandı
             printf("Skor Tahmini: %s 3-4 farkla kazanabilir\n\n", takimlar[evsahibi].takimadi);
             fprintf(dosya, "Skor Tahmini: %s 3-4 farkla kazanabilir\n\n", takimlar[evsahibi].takimadi);
-        } else if (fark >= 40) {
+                         } 
+        else if (fark >= 40) {  // Orta buyuk fark
             printf("Skor Tahmini: %s 2 farkla kazanabilir\n\n", takimlar[evsahibi].takimadi);
             fprintf(dosya, "Skor Tahmini: %s 2 farkla kazanabilir\n\n", takimlar[evsahibi].takimadi);
-        } else if (fark >= 10) {
+            takimlar[evsahibi].puan += 3;  // Ev sahibi 3 puan kazandı                 
+                             } 
+        else if (fark >= 10) {  // Kucuk fark
             printf("Skor Tahmini: %s 1 farkla kazanabilir\n\n", takimlar[evsahibi].takimadi);
             fprintf(dosya, "Skor Tahmini: %s 1 farkla kazanabilir\n\n", takimlar[evsahibi].takimadi);
-        } else {
-            printf("Skor Tahmini: Maç berabere olabilir\n\n");
-            fprintf(dosya, "Skor Tahmini: Maç berabere olabilir\n\n");
+            takimlar[evsahibi].puan += 3;  // Ev sahibi 3 puan kazandı                        
+                            }
+     else {
+            printf("Skor Tahmini: Mac berabere olabilir\n\n");
+            fprintf(dosya, "Skor Tahmini: Mac berabere olabilir\n\n");
+            takimlar[evsahibi].puan += 1;
+            takimlar[rakip].puan += 1;  // Ev sahibi ve rakip 1 puan kazandı
         }
     } 
-    else if (fark == 0) {
+    else if (fark == 0) {  // Fark sifir ise
         printf("Skor Tahmini: Mac berabere olabilir\n\n");
         fprintf(dosya, "Skor Tahmini: Mac berabere olabilir\n\n");
+        takimlar[evsahibi].puan += 1;
+        takimlar[rakip].puan += 1;  // Ev sahibi ve rakip 1 puan kazandı
     } 
-    else { 
+    else {  // Rakip daha guclu ise
         fark = -fark;
-        if (fark >= 80) {
-            printf("Skor Tahmini: %s 5-6 farkla kazanabilir\n\n", takimlar[rakip].takimadi);
-            fprintf(dosya, "Skor Tahmini: %s 5-6 farkla kazanabilir\n\n", takimlar[rakip].takimadi);
-        } else if (fark >= 60) {
+        if (fark >= 60) {  // Buyuk fark
             printf("Skor Tahmini: %s 3-4 farkla kazanabilir\n\n", takimlar[rakip].takimadi);
             fprintf(dosya, "Skor Tahmini: %s 3-4 farkla kazanabilir\n\n", takimlar[rakip].takimadi);
-        } else if (fark >= 40) {
+            takimlar[rakip].puan += 3; // Rakip 3 puan kazandı
+                         }
+             else if (fark >= 40) {  // Orta buyuk fark
             printf("Skor Tahmini: %s 2 farkla kazanabilir\n\n", takimlar[rakip].takimadi);
             fprintf(dosya, "Skor Tahmini: %s 2 farkla kazanabilir\n\n", takimlar[rakip].takimadi);
-        } else if (fark >= 10) {
+            takimlar[rakip].puan += 3; // Rakip 3 puan kazandı
+                                  }
+           else if (fark >= 10) {  // Kucuk fark
             printf("Skor Tahmini: %s 1 farkla kazanabilir\n\n", takimlar[rakip].takimadi);
             fprintf(dosya, "Skor Tahmini: %s 1 farkla kazanabilir\n\n", takimlar[rakip].takimadi);
-        }
+            takimlar[rakip].puan += 3; // Rakip 3 puan kazandı
+                                   }
     }
 }
-   //olusturulan fiksturu yazdirir
-void fiksturyazdir() {
-    FILE *dosya = fopen("fikstur.txt", "w");
 
+// Takimlari ve puanlari yazdirma
+void takimlariyazdir(FILE *dosya) {
+    printf("\n\n--- Takimlar ve Puanlar ---\n");
+    fprintf(dosya, "\n\n--- Takimlar ve Puanlar ---\n");
+
+    for (int i = 0; i < 19; i++) {
+        printf("%s: %d Puan\n", takimlar[i].takimadi, takimlar[i].puan);
+        fprintf(dosya, "%s: %d Puan\n", takimlar[i].takimadi, takimlar[i].puan);
+    }
+}
+
+// Fiksturu yazdirma
+void fiksturyazdir(FILE *dosya) {
     for (int hafta = 0; hafta < 38; hafta++) {
         printf("\n\n  %d. Hafta\n\n", hafta + 1);
         fprintf(dosya, "\n\n  %d. Hafta\n\n", hafta + 1);
         
+            
         for (int macsayisi = 0; macsayisi < 9; macsayisi++) {
             int evsahibi = eslesmeler[hafta][macsayisi].evsahibi;
             int rakip = eslesmeler[hafta][macsayisi].rakip;
@@ -132,23 +164,42 @@ void fiksturyazdir() {
             printf("%s VS %s\n", takimlar[evsahibi].takimadi, takimlar[rakip].takimadi);
             fprintf(dosya, "%s VS %s\n", takimlar[evsahibi].takimadi, takimlar[rakip].takimadi);
 
-            printf("Ev Sahibi: %s\n", takimlar[evsahibi].takimadi);
-            fprintf(dosya, "Ev Sahibi: %s\n", takimlar[evsahibi].takimadi);
-
-            printf("Rakip: %s\n", takimlar[rakip].takimadi);
-            fprintf(dosya, "Rakip: %s\n", takimlar[rakip].takimadi);
-
-            macskortahmini(evsahibi, rakip, dosya);
+            macskortahmini(evsahibi, rakip,dosya);  // Her mac skoru tahmin edilir
         }
     }
 
-    fclose(dosya);
+
+int max_puan = takimlar[0].puan; 
+int sampiyon_sayac = 0;
+
+for (int i = 1; i < 19; i++) {  // Tum takimlari kontrol et
+    if (takimlar[i].puan > max_puan) {
+        max_puan = takimlar[i].puan;  // Yeni maksimum puani belirle
+        sampiyon_sayac = i;  // Sampiyonun sayacini guncelle
+    }
+    else if (takimlar[i].puan == max_puan) {  // Puanlar esitse
+        if (takimlar[i].guc > takimlar[sampiyon_sayac].guc) {  // Guc karsilastirması
+            sampiyon_sayac = i;  // Gucu buyuk olanı guncelle
+        }
+    }
+}
+
+printf("\n\n Olasi Sampiyon: %s -> %d Puan\n", takimlar[sampiyon_sayac].takimadi, takimlar[sampiyon_sayac].puan);  // Sampiyonu ekrana yazdir
+fprintf(dosya, "\n\n Olasi Sampiyon: %s %d Puan\n", takimlar[sampiyon_sayac].takimadi, takimlar[sampiyon_sayac].puan);  // Sampiyonu dosyaya yazdir
+
 }
 
 int main() {
-    takimlarioku();
-    takimguclerinioku();
-    fiksturolustur();
-    fiksturyazdir();
+    takimlarioku();  // Takimlari oku
+    takimguclerinioku();  // Takimlarin guclerini oku
+    fiksturolustur();  // Fiksturu olustur
+    FILE *dosya = fopen("fikstur.txt", "w");  // Fiksturu dosyaya yaz
+
+    fiksturyazdir(dosya);  // Fiksturu yazdir
+    takimlariyazdir(dosya);  // Takimlari ve puanlari yazdir
+
+    fclose(dosya);  // Dosyayi kapat
+
     return 0;
 }
+
